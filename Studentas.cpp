d4965a1e05b654d76f00e2f2d68c7b37635ea710
juliz-lab <julkucin@gmail.com>
2025-11-18 22:35:30 +0200

    Update Studentas.cpp

--- Studentas.cpp
+++ Studentas.cpp
@@ -1,5 +1,18 @@
 #include "Studentas.h"
 
+double Studentas::Mediana()
+{
+    sort(paz_.begin(), paz_.end());
+    if (paz_.size() % 2 != 0)
+    {
+        return paz_[paz_.size() / 2];
+    }
+    else
+    {
+        return (float(paz_[paz_.size() / 2]) + paz_[(paz_.size() / 2) - 1]) / 2;
+    }
+}
+
 Studentas::Studentas(istringstream& iss) {
     iss >> var_ >> pav_;
     int p, suma = 0;
@@ -12,4 +25,6 @@ Studentas::Studentas(istringstream& iss) {
     egz_ = paz_.back();
     paz_.pop_back();
     gal_ = double(suma) / double(paz_.size()) * 0.4 + 0.6 * egz_;
+    med_ = Mediana() * 0.4 + 0.6 * egz_;
+    
 }
