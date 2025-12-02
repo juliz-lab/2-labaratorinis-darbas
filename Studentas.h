d0c9782ffab5032b9bcfc961c22506e2b0d01d4a
juliz-lab <julkucin@gmail.com>
2025-12-02 19:35:19 +0200

    Update Studentas.h
    Pabaigta Studentas.h dokumentacija.

--- Studentas.h
+++ Studentas.h
@@ -56,21 +56,38 @@ public:
     }
 
     double Mediana();
+    /// @return Egzamino rezultatas.
     inline int egazaminas() const { return egz_; }
+
+    /// @return Galutinis balas pagal vidurkį.
     inline double galutinis() const { return gal_; }
+
+    /// @return Galutinis balas pagal medianą.
     inline double galutinisMed() const { return med_; }
 
+    /// @brief Nustato pažymių masyvą.
     void setPaz(vector<int> &p) { paz_ = p; }
+
+    /// @brief Nustato egzamino rezultatą.
     void setEgz(int &e) { egz_ = e; }
+    /// @brief Nustato galutinį rezultatą pagal vidurkį.
     void setGal(double &g) { gal_ = g; }
+    /// @brief Nustato galutinį balą pagal medianą.
     void setMed(double &m) { med_ = m; }
 
+    /**
+     * @brief Destruktorius. Atlaisvina atmintį.
+     */
     ~Studentas()
     {
         paz_.clear();
         paz_.shrink_to_fit();
     }
 
+    /**
+     * @brief Kopijavimo konstruktorius.
+     * @param senas Kopijuojamas objektas.
+     */
     Studentas(const Studentas &senas) : Zmogus(senas)
     {
         egz_ = senas.egz_;
@@ -79,6 +96,11 @@ public:
         gal_ = senas.gal_;
     }
 
+    /**
+     * @brief Kopijavimo priskyrimo operatorius.
+     * @param senas Kopijuojamas objektas.
+     * @return Nuoroda į priskirtą objektą.
+     */
     Studentas &operator=(const Studentas &senas)
     {
         Zmogus::operator=(senas);
@@ -90,11 +112,26 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Išvedimo operatorius.
+     * @param out Išvesties srautas.
+     * @param stud Studentas, kurį norima išvesti.
+     */
     friend ostream &operator<<(ostream &out, const Studentas &stud)
     {
         out << "Vardas: " << stud.var_ << ", pavarde: " << stud.pav_ << ", egzamino pazymys: " << stud.egz_ << std::endl;
         return out;
     }
+
+    /**
+     * @brief Įvedimo operatorius. Leidžia vartotojui įvesti studento duomenis.
+     *
+     * Tikimasi įvesties formatu:
+     * vardas pavarde pažymiai (paskutinis turi būti 0) egzaminas
+     *
+     * @param in Įvesties srautas.
+     * @param stud Studentas, į kurį įrašomi duomenys.
+     */
     friend istream &operator>>(istream &in, Studentas &stud)
     {
         std::cout << "Iveskite studento varda, pavarde, pazymius (paskutini iveskite 0) ir egzamino pazymi." << std::endl;
