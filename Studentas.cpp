c1dc4d433e0d911481262259227be5f14d0287f9
juliz-lab <julkucin@gmail.com>
2025-12-02 19:40:40 +0200

    Update Studentas.cpp
    Dokumentuotas Studentas.cpp failas.

--- Studentas.cpp
+++ Studentas.cpp
@@ -1,5 +1,42 @@
 #include "Studentas.h"
+#include <iostream>
+/**
+ * @file Studentas.cpp
+ * Realizuotas Medianos skaičiavimo funkcija ir studentų nuskaitymo funkcija.
+ */
 
+ /**
+ * @brief Apskaičiuoja studento pažymių medianą.
+ *
+ * Funkcija kopijuoja pažymių vektorių, surikiuoja jį ir apskaičiuoja medianą.
+ * Jei pažymių skaičius nelyginis, grąžinama vidurinio elemento reikšmė.
+ * Jei lyginis, grąžinamas dviejų vidurinių elementų vidurkis.
+ *
+ * @return Studento pažymių mediana.
+ */
+double Studentas::Mediana()
+{
+    vector<int> temp = paz_;
+    sort(temp.begin(), temp.end());
+    if (temp.size() % 2 != 0)
+    {
+        return temp[temp.size() / 2];
+    }
+    else
+    {
+        return (temp[temp.size() / 2]) + temp[(temp.size() / 2) - 1] / 2.0;
+    }
+}
+
+/**
+ * @brief Konstruktorius, nuskaitantis studento duomenis iš istringstream srauto.
+ *
+ * Tikisi įvesties formatu: vardas pavarde [pažymiai] egzaminas.
+ * Paskutinis įvestas pažymys laikomas egzamino rezultatu.
+ * Apskaičiuojamas galutinis balas pagal vidurkį ir pagal medianą.
+ *
+ * @param iss Duomenų srautas su vieno studento informacija.
+ */
 Studentas::Studentas(istringstream& iss) {
     iss >> var_ >> pav_;
     int p, suma = 0;
@@ -12,4 +49,6 @@ Studentas::Studentas(istringstream& iss) {
     egz_ = paz_.back();
     paz_.pop_back();
     gal_ = double(suma) / double(paz_.size()) * 0.4 + 0.6 * egz_;
+    med_ = Mediana() * 0.4 + 0.6 * egz_;
+    
 }
